5f7c16ba2520ef3dcb7853ac20cd04eab4a48207
EricNavar <ericnavar@ufl.edu>
2020-05-19 13:17:32 -0400

    update comments

--- Division.java
+++ Division.java
@@ -1,5 +1,7 @@
 public class Division
 {
     //create a function that takes in two integers.
-    //It divides the first by the second and prints the equation with the result. ie: "4 / 2 = 2"
+    //It divides the first by the second and prints the equation with the result.
+    //  ie: "4 / 2 = 2"
+    //Afterwards, create a case in the switch statement in Main.java and call your function.
 }
--- Subtraction.java
+++ Subtraction.java
@@ -1,5 +1,7 @@
 public class Subtraction
 {
     //create a function that takes in two integers.
-    //It subtracts the second from the first and prints the equation with the result. ie: "2 - 3 = -1"
+    //It subtracts the second from the first and prints the equation with the result.
+    //  ie: "2 - 3 = -1"
+    //Afterwards, create a case in the switch statement in Main.java and call your function.
 }
